266e3621fec04f8e0eeacadd71e7bf6160cede41
MS-Tharu <gayantharabod@gmail.com>
2024-10-21 19:15:01 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
-MONGODB: process.env.MONGODB || "",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=i2A0EJBY#d5XQyQ0F5wqUyvYBk9GUa5ub-S8Y5DItEKyeD5w4Pj8",
+MONGODB: process.env.MONGODB || "mongodb+srv://gayanthaprabod72:ce24ome6OohZrPU6@gayan.oqqaeh3.mongodb.net/",//enter mongo db url හදන විදිය පල්ලෙහාබටන් එකක් ඇති
 };
